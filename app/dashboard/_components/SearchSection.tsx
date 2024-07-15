c95be598c09cef6ba3a734466fec748ef496610d
unknown <nachiketbagad1133@gmail.com>
2024-07-15 22:07:38 +0530

    TemplateListSection

--- app/dashboard/_components/SearchSection.tsx
+++ app/dashboard/_components/SearchSection.tsx
@@ -1,7 +1,7 @@
 import { Search } from 'lucide-react'
 import React from 'react'
 
-function SearchSection() {
+function SearchSection({onSearchInput}:any) {
   return (
     <div className='p-10 bg-gradient-to-br from-purple-500 via-purple-500 to to-blue-600
     flex flex-col justify-center items-center text-white'>
@@ -11,6 +11,7 @@ function SearchSection() {
             <div className='flex gap-2 items-center p-2 border rounded-md bg-white my-5 w-[50%]'>
                 <Search className='text-primary'/>
                 <input type="text" placeholder="Search"
+                onChange={(event)=>onSearchInput(event.target.value)}
                 className='bg-transparent w-full outline-none text-black'/>
             </div>
         </div>
