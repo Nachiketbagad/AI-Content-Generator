1e252f07dae07c5f570229a02e24b139b6622461
unknown <nachiketbagad1133@gmail.com>
2024-07-17 15:10:51 +0530

    Hompepage,History Page, payments page nd settings page pending

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react'
 import Image from 'next/image'
 import { FileClock, Home, Settings, WalletCards } from 'lucide-react'
 import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import UsageTrack from './UsageTrack'
 
 function SideNav() {
 
@@ -38,7 +40,7 @@ function SideNav() {
 
 
   return (
-    <div className='h-screen p-5 shadow-sm border bg-white'>
+    <div className='h-screen relative p-5 shadow-sm border bg-white'>
         <div className='flex justify-center'>
         <Image src={'/logo.svg'} alt='logo' width={50} height={50} />
         </div>
@@ -46,6 +48,7 @@ function SideNav() {
 
         <div className='mt-3'>
             {MenuList.map((menu, index)=>(
+                <Link href={menu.path}>
                 <div className={`flex gap-2 mb-2 p-3
                 hover:bg-primary hover:text-white rounded-lg
                 cursor-pointer items-center
@@ -54,7 +57,11 @@ function SideNav() {
                     <menu.icon className='h-6 w-6'/>
                     <h2 className='text-lg'>{menu.name}</h2>
                 </div>
+                </Link>
             ))}
+        </div>
+        <div className='absolute bottom-10 left-0 w-full'>
+            <UsageTrack/>
         </div>
     </div>
   )
